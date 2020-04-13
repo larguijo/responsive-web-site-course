761b5c52081f1ff9e7a81931648bd11f5f6593a5
Luis Josue Rodriguez Arguijo <luis.rodriguez09@telusinternational.com>
2020-04-13 16:46:14 -0600

    Adding animations and sticky navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  /*** Sticky navigation */
   $(".js--section-features").waypoint(
     function (direction) {
       if (direction == "down") {
@@ -9,4 +10,104 @@ $(document).ready(function () {
     },
     { offset: "60px" }
   );
+
+  /*** Scroll on button */
+  $(".js--scroll-to-plans").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-plans").offset().top },
+      1000
+    );
+  });
+
+  $(".js--scroll-to-start").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-features").offset().top },
+      1000
+    );
+  });
+
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000,
+            function () {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            }
+          );
+        }
+      }
+    });
+
+  /** Animations on scroll */
+  $(".js--wp-1").waypoint(
+    function () {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    { offset: "50%" }
+  );
+
+  $(".js--wp-2").waypoint(
+    function () {
+      $(".js--wp-2").addClass("animated fadeInUp");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-3").waypoint(
+    function () {
+      $(".js--wp-3").addClass("animated fadeIn");
+    },
+    { offset: "50%" }
+  );
+
+  $(".js--wp-4").waypoint(
+    function () {
+      $(".js--wp-4").addClass("animated pulse");
+    },
+    { offset: "50%" }
+  );
+
+  /* Mobile navigation */
+  $(".js--nav-icon").click(function () {
+    var nav = $(".js--main-nav");
+    var iconVisible = $(".js--nav-icon:visible");
+    var iconHidden = $(".js--nav-icon:hidden");
+    nav.slideToggle(200);
+    console.log(nav);
+    iconVisible.hide();
+    iconHidden.show();
+  });
 });
